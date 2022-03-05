80dd6c09b38f997cd1964842f67b00a8322e2075
senthamizhmaran r <Zenthamizhmaran10@gmial.com>
2022-03-05 20:13:06 +0530

    ret123

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,8 @@
+package org.emp;
+
+public class Employee {
+	public void empName () {
+		System.out.println("Employee name is R.SenthamizhMaran");
+	}
+
+}
---
+++ src/org/project/Project.java
@@ -0,0 +1,25 @@
+package org.project;
+import org.emp.Employee;
+import org.company.Company;
+import org.client.Client;
+
+public class Project {
+	public void projectName() {
+		System.out.println("project name is ADROIT ARTISTICA");
+	}
+public static void main(String[] args) {
+    Employee a = new Employee();
+    a.empName();
+    Company b = new Company();
+    b.companyName();
+    Client c = new Client();
+    c.clientName();
+    Project d = new Project();
+    d.projectName();
+    
+    
+    
+    
+}
+
+}
